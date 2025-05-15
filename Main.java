18ede617fb43c7430c72a581eb629855472777a2
LuisTsuru <luishiroyukitsuru@gmail.com>
2025-05-15 19:22:08 -0300

    leves alteracoes

--- Main.java
+++ Main.java
@@ -1,46 +1,52 @@
 import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
         }
 }
 
-class Pessoa{
-    abstract String nome;
-    abstract String cpf; //Verificar se String e um tipo decente para cpf
+abstract class Pessoa{
+    private String nome;
+    private String cpf; //Verificar se String e um tipo decente para cpf
 }
+
 class Funcionarios extends Pessoa{
-    String cargo;
-    String grupo;
+    private String cargo;
+    private String grupo;
     ArrayList<Ticket> ticket_individual; // Ideia para criar caixa entrada para cada funcionario e seus tickets
 
 }
 
-class Prioridade{
-    int gravidade;
-    //Ideia: implementar calculo de prioridade e servico afetado
-}
-class Ticket{
-    String status;
-    String emissor;
-    Date data; //Verificar tipo para data
-    int id_ticket; 
-    String titulo;
-    String descricao;
-    Funcionarios responsavel = null;
-    Prioridade prioridade;
+class Prioridade {
+private int prioridade;
+private String tipo_afetado; //Afeta pessoa, servico ou departamento
+
 
 }
+
+class Ticket{
+    private int id_ticket;
+    private String emissor;
+    private Date data; //Verificar tipo para data
+    private String status; 
+    private String titulo;
+    private String descricao;
+    private String tipo_servico;
+    private Funcionarios responsavel = null;
+    private Prioridade gravidade;
+
+}
+
 class CaixaEntrada{
     ArrayList<Ticket>;
     //Criar funcao para organizar por prioridade (Organizacao);
 }
-class Resposaveis{
-    String nome_resp;
-    Date ttr; //defirinir tipo para data (ttr eh o tempo para conclusao limite de ticket)
+
+class Resposaveis  Funcionarios{ //Descobrir se devo usar extend ou implement para o responsavel herdar o nome para usar em nome_resp
+    String nome_resp = ;
+    Date ttr; //defininir tipo para data (ttr eh o tempo para conclusao limite de ticket)
 
 }
 
@@ -58,6 +64,7 @@ class Financeiro extends Resposaveis{
     ArrayList<Funcionarios> resp_finan;
     ArrayList<Ticket> ticket_finan;
 }
+
 class  Marketing extends Resposaveis{
     ArrayList<Funcionarios> resp_mark;
     ArrayList<Ticket> ticket_mark;
