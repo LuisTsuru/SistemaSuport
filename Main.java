85b08e4553f6d9fd140700d2ac802078f0c2a62b
LuisTsuru <luishiroyukitsuru@gmail.com>
2025-05-15 21:31:45 -0300

    correcoes leves e adicao de print menu

--- Main.java
+++ Main.java
@@ -1,14 +1,50 @@
 import java.util.ArrayList;
-
+import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void printMenu(){
+        Scanner s= new Scanner(System.in);
+        System.out.println("========== M E N U ==========");
+        System.out.println("1 - Criar Ticket\n2- Visualizar Caixa de entrada\n\3- Atribuir Ticket a time/Individuo\n4 - Visualizar Caixa Individual\n5 - Visualizar Caixa de Time");
+        System.out.println("=============================");
+        System.out.print("Digite opcao: ");
+        int opcao = s.nextInt();
+
+        switch(opcao){
+            case 1:
+                //funcao1
+                break;
+            case 2:
+                //funcao2
+                break;
+            case 3:
+                //funcao3
+                break;
+            case 4:
+                //funcao4
+                break;
+            case 5:
+                //funcao5
+                break;
+            default:
+                System.out.println("Opcao invalida :()");
 
         }
+
+    }
+
+    public static void main(String[] args) {
+        printMenu();
+
+    }
 }
 
+
+
 abstract class Pessoa{
-    private String nome;
+    String nome;
     private String cpf; //Verificar se String e um tipo decente para cpf
 }
 
@@ -20,8 +56,8 @@ class Funcionarios extends Pessoa{
 }
 
 class Prioridade {
-private int prioridade;
-private String tipo_afetado; //Afeta pessoa, servico ou departamento
+    private int prioridade;
+    private String tipo_afetado; //Afeta pessoa, servico ou departamento
 
 
 }
@@ -30,7 +66,7 @@ class Ticket{
     private int id_ticket;
     private String emissor;
     private Date data; //Verificar tipo para data
-    private String status; 
+    private String status;
     private String titulo;
     private String descricao;
     private String tipo_servico;
@@ -39,19 +75,24 @@ class Ticket{
 
 }
 
-interface OrganizaTicket{
-    void organizarPorPrioridade();
-}
+//interface OrganizaTicket{
+//    void organizarPorPrioridade();
+//}
+//
+//class CaixaEntrada implements OrganizaTicket {
+//    ArrayList<Ticket> caixa_de_entrada;
+//    organizarPorPrioridade();
+//
+//    @Override
+//    public void organizarPorPrioridade() {
+//
+//    }
+//}
 
-class CaixaEntrada implement OrganizaTicket {
-    ArrayList<Ticket> caixa_de_entrada;
-    organizarPorPrioridade();
-}
 
 
-
-class Resposaveis  Funcionarios{ //Descobrir se devo usar extend ou implement para o responsavel herdar o nome para usar em nome_resp
-    String nome_resp = ;
+class Resposaveis extends Funcionarios{ //Descobrir se devo usar extend ou implement para o responsavel herdar o nome para usar em nome_resp
+    String nome_resp = nome;
     Date ttr; //defininir tipo para data (ttr eh o tempo para conclusao limite de ticket)
 
 }
